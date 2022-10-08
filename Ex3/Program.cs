912c22de307a10da57e6aac8d54ba1751501bc73
Saglara <nohaevass@yandex.ru>
2022-10-08 20:40:42 +0300

    Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.

---
+++ Ex3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 45: Напишите программу, которая будет создавать 
+//копию заданного массива с помощью поэлементного копирования.
+
+Console.Clear();  
+int [] array =FillArr(5,1,10);
+Console.WriteLine("Массив: "+"[ "+ string.Join(",",array)+" ]");
+Console.WriteLine("Копия массива: "+"[ "+ string.Join(",",CopyArr(array))+" ]");
+
+int [] FillArr(int size, int min, int max)
+{
+    int [] filledArray = new int [size];
+    for (int i=0; i<size; i++)
+    {
+        filledArray[i]=new Random().Next(min,max+1);
+    }
+    return filledArray;
+}
+
+
+int [] CopyArr(int [] inputArray)
+{
+    int [] CopyedArr = new int [inputArray.Length];
+    for (int i=0; i<inputArray.Length; i++)
+    {
+        CopyedArr[i]=inputArray[i];
+    }
+    return CopyedArr;
+}
